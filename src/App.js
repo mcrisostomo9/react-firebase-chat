d1bc00f210743e9d0141508cb1276aa092e026fe
mcrisostomo9 <mcrisostomo9@gmail.com>
2017-06-19 11:53:51 -0700

    added Header placeholder component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header/Header';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Placeholder for components
+      <div className="container">
+        <Header/>
       </div>
     );
   }
---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navbar, NavItem } from 'react-materialize'
+
+const Header = () => {
+    return (
+        <Navbar brand="Firebase Chat" right>
+            <NavItem href="#">Placeholder</NavItem>
+            <NavItem href="#">More Placeholder</NavItem>
+        </Navbar>
+        )
+};
+
+export default Header;
